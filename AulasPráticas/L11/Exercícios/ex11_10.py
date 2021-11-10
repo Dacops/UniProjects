d382bad4761410662e6615d1dc0d9e6e12903034
Dacops <79417054+Dacops@users.noreply.github.com>
2021-11-10 23:38:55 +0000

    ex11_10 fix
    lst to int

--- AulasPráticas/L11/Exercícios/ex11_10.py
+++ AulasPráticas/L11/Exercícios/ex11_10.py
@@ -1,7 +1,7 @@
-import ex11_8
+import ex11_8, functools
 
 def apenas_digitos_impares(n):
-    return list(filter(lambda x:x%2!=0, ex11_8.lista_digitos(n)))
+    return int(functools.reduce(lambda x,y:str(x)+str(y) ,list(filter(lambda x:x%2!=0, ex11_8.lista_digitos(n)))))
 
  
 if __name__=='__main__':
